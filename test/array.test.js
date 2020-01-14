b50d722886bbfcadcc5e4c1d42d71b4bf4e42334
James Blackledge <james.blackledge@ada.ac.uk>
2020-01-14 15:17:14 +0000

    Finished exercises.

--- test/array.test.js
+++ test/array.test.js
@@ -1,31 +1,106 @@
-// Here are some tests for some of JavaScript's built-in Array methods
-var chai = require('chai')
-var expect = chai.expect
+var chai = require('chai');
+var expect = chai.expect;
 
 describe('Array', function() {
+  //PUSH
   describe('#push()', function() {
     it('should add an item to the array', function() {
-      var array = [1, 2, 3, 4]
-      array.push(5)
-      expect(array).to.deep.equal([1, 2, 3, 4, 5])
-    })
+      var array = [1, 2, 3, 4];
+      array.push(5);
+
+      expect(array).to.deep.equal([1, 2, 3, 4, 5]);
+    });
 
     it('should alter the length property', function() {
-      var array = [1, 1, 1, 1]
-      array.push(1)
-      expect(array.length).to.equal(5)
-    })
+      var array = [1, 1, 1, 1];
+      array.push(1);
 
+      expect(array.length).to.equal(5);
+    });
+  });
+
+
+  //POP
+  describe('#pop()', function() {
     it('should remove the last item from the array', function() {
-      var array = [1, 2, 3, 4, 5]
-      array.pop()
-      expect(array).to.deep.equal([1, 2, 3, 4])
+      var array = [1, 2, 3, 4, 5];
+      array.pop();
+
+      expect(array).to.deep.equal([1, 2, 3, 4]);
     })
 
     it('should alter the length property', function() {
-      var array = [1, 1, 1, 1, 1]
-      array.pop()
-      expect(array.length).to.equal(4)
+      var array = [1, 1, 1, 1, 1];
+      array.pop();
+
+      expect(array.length).to.equal(4);
+    });
+  });
+
+
+  //INDEXOF
+  describe('#indexOf()', function() {
+    it('should return the index of 3', function() {
+      var array = [1, 2, 3, 4, 5];
+
+      expect(array.indexOf(3)).to.equal(2);
+    });
+
+    it('should return -1', function() {
+      var array = [1, 2, 3, 4, 5];
+
+      expect(array.indexOf(6)).to.equal(-1);
+    });
+  });
+
+
+  //JOIN
+  describe('#join()', function() {
+    it('should join the array on ,', function() {
+      var array = [1, 2, 3, 4, 5];
+
+      expect(array.join(", ")).to.equal("1, 2, 3, 4, 5");
+    });
+
+    it('should join the array on -', function() {
+      var array = [1, 2, 3, 4, 5];
+
+      expect(array.join("-")).to.equal("1-2-3-4-5");
+    });
+  });
+
+
+  //SHIFT
+  describe('#shift()', function() {
+    it('should remove the first element', function() {
+      var array = [1, 2, 3, 4, 5];
+      array.shift();
+
+      expect(array).to.deep.equal([2, 3, 4, 5]);
+    });
+
+    it('should return the first element', function() {
+      var array = [1, 2, 3, 4, 5];
+      var firstElement = array.shift();
+
+      expect(firstElement).to.equal(1);
+    });
+  });
+
+
+  //UNSHIFT
+  describe('#unshift()', function() {
+    it('should change the length of the array', function() {
+      var array = [1, 2, 3, 4, 5];
+
+      expect(array.unshift(6, 7)).to.equal(7);
     })
-  })
-})
+
+    it('should add to the start of the array', function() {
+      var array = [1, 2, 3, 4, 5];
+      array.unshift(6, 7);
+
+      expect(array).to.deep.equal([6, 7, 1, 2, 3, 4, 5]);
+    });
+  });
+});
--- test/coolculator.test.js
+++ test/coolculator.test.js
@@ -6,103 +6,175 @@ var Coolculator = require('../lib/coolculator');
 describe('Coolculator', function() {
   mm = new Coolculator();
 
-  it('should add', function() {
-    result = mm.add(2, 3);
-    expect(result).to.equal(5);
+  //ADD
+  describe('#add()', function() {
+    it('should add', function() {
+      result = mm.add(2, 3);
+      expect(result).to.equal(5);
+    });
+
+    it('should add variable', function() {
+      result = mm.add(2, 3, 4, 5, 6);
+      expect(result).to.equal(20);
+    });
   });
 
-  it('should multiply', function() {
-    result = mm.multiply(2, 3);
-    expect(result).to.equal(6);
+
+  //MULTIPLY
+  describe('#multiply()', function() {
+    it('should multiply', function() {
+      result = mm.multiply(2, 3);
+      expect(result).to.equal(6);
+    });
   });
 
-  it('should subtract', function() {
-    result = mm.subtract(3, 2);
-    expect(result).to.equal(1);
+
+  //SUBTRACT
+  describe('#subtract()', function() {
+    it('should subtract', function() {
+      result = mm.subtract(3, 2);
+      expect(result).to.equal(1);
+    });
   });
 
-  it('should divide', function() {
-    result = mm.divide(4, 2);
-    expect(result).to.equal(2);
+
+  //DIVIDE
+  describe('#divide()', function() {
+    it('should divide', function() {
+      result = mm.divide(4, 2);
+      expect(result).to.equal(2);
+    });
   });
 
-  it('should return the highest', function() {
-    result = mm.highest(4, 8);
-    expect(result).to.equal(8);
+
+  //HIGHEST
+  describe('#highest()', function() {
+    it('should return the highest', function() {
+      result = mm.highest(4, 8);
+      expect(result).to.equal(8);
+    });
   });
 
-  it('should return the lowest', function() {
-    result = mm.lowest(4, 8);
-    expect(result).to.equal(4);
+
+  //LOWEST
+  describe('#lowest()', function() {
+    it('should return the lowest', function() {
+      result = mm.lowest(4, 8);
+      expect(result).to.equal(4);
+    });
   });
 
-  it('should double', function() {
-    result = mm.double(4);
-    expect(result).to.equal(8);
+
+  //DOUBLE
+  describe('#double()', function() {
+    it('should double', function() {
+      result = mm.double(4);
+      expect(result).to.equal(8);
+    });
   });
 
-  it('should square', function() {
-    result = mm.square(4);
-    expect(result).to.equal(16);
+
+  //SQUARE
+  describe('#square()', function() {
+    it('should square', function() {
+      result = mm.square(4);
+      expect(result).to.equal(16);
+    });
   });
 
-  it('should raise', function() {
-    result = mm.raise(4, 3);
-    expect(result).to.equal(64);
+
+  //RAISE
+  describe('#raise()', function() {
+    it('should raise', function() {
+      result = mm.raise(4, 3);
+      expect(result).to.equal(64);
+    });
+  }); 
+
+
+  //ISNEGATIVE
+  describe('#isNegative()', function() {
+    it('is negative', function() {
+      result = mm.isNegative(-16);
+      expect(result).to.equal(true);
+    });
+
+    it('is not negative', function() {
+      result = mm.isNegative(16);
+      expect(result).to.equal(false);
+    });
   });
 
-  it('is negative', function() {
-    result = mm.isNegative(-16);
-    expect(result).to.equal(true);
+
+  //ISPOSITIVE
+  describe('#isPositive()', function() {
+    it('is positive', function() {
+      result = mm.isPositive(24);
+      expect(result).to.equal(true);
+    });
+
+    it('is not positive', function() {
+      result = mm.isPositive(-24);
+      expect(result).to.equal(false);
+    });
   });
 
-  it('is not negative', function() {
-    result = mm.isNegative(16);
-    expect(result).to.equal(false);
+
+  //ISCOOL
+  describe('#isCool()', function() {
+    it('is cool', function() {
+      result = mm.isCool(548695);
+      expect(result).to.equal(true);
+    });
+
+    it('is not cool', function() {
+      result = mm.isCool(548696);
+      expect(result).to.equal(false);
+    });
   });
 
-  it('is positive', function() {
-    result = mm.isPositive(24);
-    expect(result).to.equal(true);
+
+  //SUM
+  describe('#sum()', function() {
+    it('should sum all numbers together', function() {
+      result = mm.sum([1, 2, 3, 4]);
+      expect(result).to.equal(10);
+    });
   });
 
-  it('is not positive', function() {
-    result = mm.isPositive(-24);
-    expect(result).to.equal(false);
+
+  //MULTIPLYARRAY
+  describe('#multiplyArray()', function() {
+    it('should multiply all numbers together', function() {
+      result = mm.multiplyArray([1, 2, 3, 4]);
+      expect(result).to.equal(24);
+    });
   });
 
-  it('is cool', function() {
-    result = mm.isCool(548695);
-    expect(result).to.equal(true);
+
+  //MEAN
+  describe('#mean()', function() {
+    it('should average', function() {
+      result = mm.mean([1, 2, 3, 4]);
+      expect(result).to.equal(2.5);
+    });
   });
 
-  it('is not cool', function() {
-    result = mm.isCool(548696);
-    expect(result).to.equal(false);
+
+  //FACTORIAL
+  describe('#factorial()', function() {
+    it('should factorial', function() {
+      result = mm.factorial(5);
+      expect(result).to.equal(120);
+    });
   });
 
-  it('should sum all numbers together', function() {
-    result = mm.sum([1, 2, 3, 4]);
-    expect(result).to.equal(10);
-  });
 
-  it('should multiply all numbers together', function() {
-    result = mm.multiplyArray([1, 2, 3, 4]);
-    expect(result).to.equal(24);
-  });
-
-  it('should average', function() {
-    result = mm.mean([1, 2, 3, 4]);
-    expect(result).to.equal(2.5);
-  });
-
-  it('should factorial', function() {
-    result = mm.factorial(5);
-    expect(result).to.equal(120);
-  });
-
-  it('should random', function() {
-    result = mm.random(5);
-    expect(result).to.below(5);
+  //RANDOM
+  describe('#random()', function() {
+    it('should random', function() {
+      result = mm.random(5);
+      expect(result).to.below(5);
+    });
   });
 });
